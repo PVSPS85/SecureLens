46c7e974dc374484b8dd127d87f25a91543c6127
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:57:26 +0530

    Add WhoisUtils class with domain age calculation

--- securityEngine/analyzers/whois/whois.utils.js
+++ securityEngine/analyzers/whois/whois.utils.js
@@ -1,1 +1,18 @@
+class WhoisUtils {
+  /**
+   * Computes age in days from domain creation timestamp.
+   * @param {string|Date} creationDate 
+   */
+  static calculateDomainAgeDays(creationDate) {
+    if (!creationDate) return null;
+    const created = new Date(creationDate);
+    if (isNaN(created.getTime())) return null;
 
+    const diffMs = Date.now() - created.getTime();
+    if (diffMs < 0) return 0;
+
+    return Math.floor(diffMs / (1000 * 60 * 60 * 24));
+  }
+}
+
+module.exports = WhoisUtils;
