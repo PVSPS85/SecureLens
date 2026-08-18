f2026582a3e17bf734f27aea5dd3b8821e83aea2
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:20:09 +0530

    Update caching.js

--- securityEngine/utils/caching.js
+++ securityEngine/utils/caching.js
@@ -1,1 +1,29 @@
+class CacheManager {
+  constructor(defaultTtlMs = 300000) { // 5 minutes default
+    this.cache = new Map();
+    this.defaultTtl = defaultTtlMs;
+  }
 
+  set(key, value, ttlMs = this.defaultTtl) {
+    const expiresAt = Date.now() + ttlMs;
+    this.cache.set(key, { value, expiresAt });
+  }
+
+  get(key) {
+    const item = this.cache.get(key);
+    if (!item) return null;
+
+    if (Date.now() > item.expiresAt) {
+      this.cache.delete(key);
+      return null;
+    }
+
+    return item.value;
+  }
+
+  clear() {
+    this.cache.clear();
+  }
+}
+
+module.exports = CacheManager;
