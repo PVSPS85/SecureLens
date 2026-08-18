667fcc86533ccdec97ad66634444fa1d29e8aeb7
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:03:54 +0530

    Add ResourceLimits class with timeout and truncation methods

--- securityEngine/security/resource-limits.js
+++ securityEngine/security/resource-limits.js
@@ -1,1 +1,33 @@
+class ResourceLimits {
+  /**
+   * Wraps a promise with a hard timeout limit.
+   * @param {Promise<any>} promise 
+   * @param {number} ms - Timeout in milliseconds
+   * @param {string} taskName 
+   */
+  static withTimeout(promise, ms = 5000, taskName = 'Operation') {
+    let timer;
+    const timeoutPromise = new Promise((_, reject) => {
+      timer = setTimeout(() => {
+        reject(new Error(`${taskName} timed out after ${ms}ms`));
+      }, ms);
+    });
 
+    return Promise.race([promise, timeoutPromise]).finally(() => {
+      clearTimeout(timer);
+    });
+  }
+
+  /**
+   * Truncates dynamic buffers/responses to prevent memory overflow.
+   * @param {string} data 
+   * @param {number} maxBytes 
+   */
+  static truncateString(data, maxBytes = 1000000) {
+    if (typeof data !== 'string') return '';
+    return data.length > maxBytes ? data.slice(0, maxBytes) : data;
+  }
+}
+
+module.ResourceLimits = ResourceLimits;
+module.exports = ResourceLimits;
