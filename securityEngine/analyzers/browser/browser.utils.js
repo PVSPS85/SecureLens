aa3f3b054edbf262e84119fc90c673be437e2e31
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:52:40 +0530

    Update browser.utils.js

--- securityEngine/analyzers/browser/browser.utils.js
+++ securityEngine/analyzers/browser/browser.utils.js
@@ -1,1 +1,30 @@
+class BrowserUtils {
+  /**
+   * Evaluates input fields and forms present on a page context.
+   * @param {Array<Object>} formElements 
+   */
+  static extractFormSecurityDetails(formElements = []) {
+    let passwordInputs = 0;
+    let sensitiveInputs = 0;
+    let hasExternalFormAction = false;
 
+    for (const form of formElements) {
+      if (form.action && /^https?:\/\//i.test(form.action)) {
+        hasExternalFormAction = true;
+      }
+      for (const inputType of form.inputTypes || []) {
+        if (inputType === 'password') passwordInputs++;
+        if (['credit-card', 'ssn', 'cvv', 'email'].includes(inputType)) sensitiveInputs++;
+      }
+    }
+
+    return {
+      passwordInputs,
+      sensitiveInputs,
+      hasExternalFormAction,
+      isCredentialHarvestingRisk: passwordInputs > 0 && hasExternalFormAction
+    };
+  }
+}
+
+module.exports = BrowserUtils;
