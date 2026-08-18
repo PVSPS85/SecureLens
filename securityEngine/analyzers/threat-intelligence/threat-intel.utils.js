74000ce34ec6429a4db1fbd763ca83942c6482fb
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:55:18 +0530

    Update threat-intel.utils.js

--- securityEngine/analyzers/threat-intelligence/threat-intel.utils.js
+++ securityEngine/analyzers/threat-intelligence/threat-intel.utils.js
@@ -1,1 +1,18 @@
+class ThreatIntelUtils {
+  /**
+   * Calculates consensus risk level from engines array.
+   * @param {number} malicious 
+   * @param {number} totalEngines 
+   */
+  static calculateConsensus(malicious = 0, totalEngines = 1) {
+    if (totalEngines <= 0) return { ratio: 0, isConsensusMalicious: false };
 
+    const ratio = Number((malicious / totalEngines).toFixed(2));
+    return {
+      ratio,
+      isConsensusMalicious: ratio >= 0.05 || malicious >= 3
+    };
+  }
+}
+
+module.exports = ThreatIntelUtils;
