41cbe497af469ac9d05d7928d26e6d1aa7904887
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:22:50 +0530

    Update nrd.scheduler.js

--- securityEngine/nrd/nrd.scheduler.js
+++ securityEngine/nrd/nrd.scheduler.js
@@ -1,1 +1,31 @@
+const Logger = require('../utils/logger');
 
+class NRDScheduler {
+  constructor(nrdProcessor, fetchFeedFn, intervalMs = 3600000) { // Default 1 hour
+    this.processor = nrdProcessor;
+    this.fetchFeedFn = fetchFeedFn;
+    this.intervalMs = intervalMs;
+    this.timer = null;
+  }
+
+  start() {
+    Logger.info('Starting NRD Cron Scheduler...');
+    this.timer = setInterval(async () => {
+      try {
+        const rawFeed = await this.fetchFeedFn();
+        await this.processor.processBatch(rawFeed);
+      } catch (err) {
+        Logger.error('Scheduled NRD job failed:', err);
+      }
+    }, this.intervalMs);
+  }
+
+  stop() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      Logger.info('Stopped NRD Scheduler.');
+    }
+  }
+}
+
+module.exports = NRDScheduler;
