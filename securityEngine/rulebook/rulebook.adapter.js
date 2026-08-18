530d64f211a241a2ffa5ee7892e83511b161eae8
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:23:08 +0530

    Update rulebook.adapter.js

--- securityEngine/rulebook/rulebook.adapter.js
+++ securityEngine/rulebook/rulebook.adapter.js
@@ -1,1 +1,10 @@
+const RulebookEngine = require('./rulebook.engine');
 
+class RulebookAdapter {
+  static evaluateEvidence(evidenceContract) {
+    const rawPayload = evidenceContract.toJSON ? evidenceContract.toJSON() : evidenceContract;
+    return RulebookEngine.evaluate(rawPayload);
+  }
+}
+
+module.exports = RulebookAdapter;
