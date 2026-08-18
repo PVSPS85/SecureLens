a767292f3daa159f3498ba0a8eafaced00fc7392
Nishanth Sai <nanth2810@gmail.com>
2026-08-18 21:18:52 +0530

    Implement NRDDeduplicator class for domain filtering
    Added NRDDeduplicator class to filter duplicate domains.

--- securityEngine/nrd/nrd.deduplicator.js
+++ securityEngine/nrd/nrd.deduplicator.js
@@ -1,1 +1,27 @@
+class NRDDeduplicator {
+  constructor() {
+    this.seenDomains = new Set();
+  }
 
+  /**
+   * Filters out already processed domains from batch array.
+   * @param {Array<string>} domains 
+   * @returns {Array<string>} Unseen domains
+   */
+  filterDuplicates(domains) {
+    const unique = [];
+    for (const domain of domains) {
+      if (!this.seenDomains.has(domain)) {
+        this.seenDomains.add(domain);
+        unique.push(domain);
+      }
+    }
+    return unique;
+  }
+
+  clearCache() {
+    this.seenDomains.clear();
+  }
+}
+
+module.exports = NRDDeduplicator;
